7a504e01eeda6b0efe3d1f047a99473a19f27c2a
Red-Goats <104803035+Red-Goats@users.noreply.github.com>
2022-12-06 13:28:25 -0500

    Add files via upload

---
+++ simplecryptoML.py
@@ -0,0 +1,24 @@
+# Import the necessary libraries
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+# Load the data into a Pandas DataFrame
+data = pd.read_csv("crypto_data.csv")
+
+# Split the data into input features and target variable
+X = data.drop("price", axis=1)
+y = data["price"]
+
+# Create a random forest regressor and train it on the data
+model = RandomForestRegressor()
+model.fit(X, y)
+
+# Use the trained model to make predictions on new data
+new_data = pd.read_csv("new_crypto_data.csv")
+predictions = model.predict(new_data)
+
+# This code uses a random forest regressor to train a model on the input data and then make
+# predictions on new data. You can modify the code to use different machine learning
+# algorithms or different types of input data to improve the accuracy of the predictions.
+#
+#
